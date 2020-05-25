bffc873a34a4ae6b39b3cbc3dd412ed10e970781
siroshan <siroshang@gmail.com>
2020-05-26 03:21:53 +0530

    login page

--- app/src/main/java/lk/xtracheese/swiftsalon/MainActivity.java
+++ app/src/main/java/lk/xtracheese/swiftsalon/MainActivity.java
@@ -2,13 +2,31 @@ package lk.xtracheese.swiftsalon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    TextView txtCreateAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        txtCreateAcc = findViewById(R.id.btn_create_account);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
