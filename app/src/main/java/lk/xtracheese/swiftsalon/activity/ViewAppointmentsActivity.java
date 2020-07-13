70d44e047fd0530435a484e5cd932e92f4705643
siroshan <siroshang@gmail.com>
2020-07-14 02:38:46 +0530

    layout changes

---
+++ app/src/main/java/lk/xtracheese/swiftsalon/activity/ViewAppointmentsActivity.java
@@ -0,0 +1,13 @@
+package lk.xtracheese.swiftsalon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewAppointmentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_appointments);
+    }
+}
